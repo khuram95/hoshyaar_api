8a2090ce734ed07a263ed3693b29c80e43e53606
khuram95 <khurams1995@gmail.com>
2019-03-02 12:47:47 +0500

    remove home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :models
   # get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
   namespace :api do
     namespace :v1 do
-      resources :homes, only: :index
+      resources :users, only: :index
     end
   end
 end
