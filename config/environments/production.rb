4831b9ce8b79b10ee664bc8c477ae3ca3cdec1c2
khuram95 <khurams1995@gmail.com>
2019-04-26 13:15:02 +0500

    set url for production in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = ENV.fetch('DEFAULT_URL')
+  Rails.application.routes.default_url_options[:host] = ENV.fetch('DEFAULT_URL') || 'https://hoshyaar.herokuapp.com/'
 end
