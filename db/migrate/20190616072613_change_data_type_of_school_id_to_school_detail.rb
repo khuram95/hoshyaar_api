639fd5a987cfcc62be4e0719dc661037b60b2ce6
khuram95 <khurams1995@gmail.com>
2019-06-16 19:47:01 +0500

    change migration file

--- db/migrate/20190616072613_change_data_type_of_school_id_to_school_detail.rb
+++ db/migrate/20190616072613_change_data_type_of_school_id_to_school_detail.rb
@@ -1,11 +1,11 @@
 class ChangeDataTypeOfSchoolIdToSchoolDetail < ActiveRecord::Migration[5.2]
-  # def change
-  # end
-  def self.up
-    change_column :school_details, :school_id, :string
-  end
+  def change
+    def self.up
+      change_column :school_details, :school_id, :string
+    end
 
-  def self.down
-    change_column :school_details, :school_id, :integer
+    def self.down
+      change_column :school_details, :school_id, :integer
+    end
   end
 end
